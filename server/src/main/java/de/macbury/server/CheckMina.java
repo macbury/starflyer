de72f0056a4e48017239bc1d6584a847dc2d6454
Arkadiusz Buras <macbury@gmail.com>
2016-08-16 14:55:51 +0200

    Api communication

---
+++ server/src/main/java/de/macbury/server/CheckMina.java
@@ -0,0 +1,72 @@
+package de.macbury.server;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.Date;
+
+/**
+ * Created by macbury on 16.08.16.
+ */
+public class CheckMina {
+  public static void main(String[] args) {
+    IoAcceptor acceptor = new NioSocketAcceptor();
+    acceptor.getFilterChain().addLast( "logger", new LoggingFilter() );
+    acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
+
+    acceptor.setHandler(  new TimeServerHandler() );
+    acceptor.getSessionConfig().setReadBufferSize( 2048 );
+    acceptor.getSessionConfig().setIdleTime( IdleStatus.BOTH_IDLE, 10 );
+
+    try {
+      acceptor.bind( new InetSocketAddress(3000) );
+      while(acceptor.isActive()) {
+        try {
+          Thread.sleep(100);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      acceptor.dispose(false);
+    }
+  }
+
+  private static class TimeServerHandler extends IoHandlerAdapter {
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+      System.out.println( "IDLE " + session.getIdleCount( status ));
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+      cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+      String str = message.toString();
+      if( str.trim().equalsIgnoreCase("quit") ) {
+        session.closeNow();
+        return;
+      }
+
+      Date date = new Date();
+      session.write( date.toString() );
+      System.out.println("Message written...");
+    }
+  }
+}
--- server/src/main/java/de/macbury/server/StarflyerServer.java
+++ server/src/main/java/de/macbury/server/StarflyerServer.java
@@ -2,9 +2,6 @@ package de.macbury.server;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.esotericsoftware.kryonet.Connection;
-import com.esotericsoftware.kryonet.Listener;
-import com.esotericsoftware.kryonet.Server;
 import de.macbury.SharedConsts;
 import de.macbury.server.db.Database;
 import de.macbury.server.utils.ServerArgs;
@@ -18,30 +15,15 @@ public class StarflyerServer implements ApplicationListener {
   private static final String TAG = "StarflyerServer";
   private final Database database;
   private final ServerArgs args;
-  private final Server server;
 
   public StarflyerServer(Database database, ServerArgs serverArgs) {
     this.database = database;
     this.args     = serverArgs;
-    this.server   = new Server();
-    server.addListener(new Listener() {
-      @Override
-      public void received(Connection connection, Object object) {
-        super.received(connection, object);
-        Gdx.app.log("RECIVED", object.toString());
-      }
-    });
   }
 
   @Override
   public void create() {
     Gdx.app.log(TAG, "Initializing...");
-    server.start();
-    try {
-      server.bind(SharedConsts.PORT);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 
   @Override
